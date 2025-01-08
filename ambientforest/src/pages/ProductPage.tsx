7f45648e3642e1faee959429dee4343d8ba3bd46
Alex Diyan <alexdiyan.ad@gmail.com>
2025-01-08 15:21:21 +0000

    Hide you might like code

--- ambientforest/src/pages/ProductPage.tsx
+++ ambientforest/src/pages/ProductPage.tsx
@@ -1,9 +1,9 @@
-import { Heading, HStack, Image, Text, VStack } from '@chakra-ui/react'
+import { VStack } from '@chakra-ui/react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import ProductPageItem from '../components/ProductPageItem'
 import candle from "../assets/candle.png"
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { Product } from '../types/types'
 import { getProductByTypeAndIdFromDynamoDB } from '../api/awsApi'
@@ -61,9 +61,9 @@ const ProductPage = () => {
       <VStack w={"100vw"} >
         <Navbar/>
         <VStack w={"100%"} gap={10} mt={{base: "6em", lg: "10em"}} mb={5}>
-          {/* # TODO add ingredients disclosure */}
           <ProductPageItem product={product}/>
-          <Heading size={"3xl"} letterSpacing={"1px"} fontFamily={"Novecento"} color={"black"}>You Might Also Like</Heading>
+          {/* TODO: add this back when there more products */}
+          {/* <Heading size={"3xl"} letterSpacing={"1px"} fontFamily={"Novecento"} color={"black"}>You Might Also Like</Heading>
           <HStack w={"100%"} justify={"center"} gap={{base: 10, lg: 14}} wrap={"wrap"}>
             {[1, 2, 3, 4].map((item) => {
               return (
@@ -71,14 +71,14 @@ const ProductPage = () => {
                   <Link to={"/product/candle2"}>
                     <Image w={"100%"} aspectRatio={"1/1"}  objectFit={"contain"} src={candle} alt="Image of candle/tealight/giftset for sale"/>
                     <VStack w={"100%"} bg={"--gold"} gap={0} py={1}>
-                      <Heading size={{base: "lg", xl: "2xl"}} letterSpacing={"1px"} fontFamily={"Nazare"} >Winter Forest</Heading>
+                      <Heading size={{base: "lg", xl: "2xl"}} letterSpacing={"1px"} fontFamily={"Nazare"}>Winter Forest</Heading>
                       <Text fontSize={{base: "md", xl: "xl"}}>£79.99 GBP</Text>
                     </VStack>
                   </Link>
                 </VStack>
               )
             })}
-          </HStack>
+          </HStack> */}
         </VStack>
         <Footer/>
       </VStack>
